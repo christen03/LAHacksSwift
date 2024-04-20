4bd07cbab2b86a4d9efc289da24bbd2831d23deb
christenx <99223047+christen03@users.noreply.github.com>
2024-04-20 00:47:39 -0700

    Initial Commit

---
+++ ClimaxApp Watch App/ClimaxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaxAppApp.swift
+//  ClimaxApp Watch App
+//
+//  Created by Christen Xie on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ClimaxApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClimaxApp/ClimaxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaxAppApp.swift
+//  ClimaxApp
+//
+//  Created by Christen Xie on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct ClimaxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
